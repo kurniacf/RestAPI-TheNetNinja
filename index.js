6d008a6863b5f9ea1e08e6612f3f9bdbfd5c8587
kurniacf <kcf170202@gmail.com>
2022-01-01 12:00:22 +0700

    error handling

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 // Set Express
 const app = express();
+
+// Connect MongoDB
+mongoose.connect("mongodb://localhost/tutorialrest");
+mongoose.Promise = global.Promise;
 
 app.use(express.json());
 //app.use(express.urlencoded());
 
 // Route
 app.use("/api", require("./routes/api"));
+
+// Error handling middleware
+app.use(function(err, req, res, next){  
+    res.status(422).send({
+        error: err.message
+    });
+});
 
 
 // PORT
--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,27 @@
 const express = require('express');
+const Profile = require('../models/profileSchema');
 const router = express.Router();
 
 // GET
-router.get("/profile", function(req, res){
+router.get("/profile", function(req, res, next){
     res.send({type: "GET"});
 });
 
 // POST
-router.post("/profile", function(req, res){
-    console.log(req.body);
-    res.send({
-        type: "POST",
-        name: req.body.name,
-        city: req.body.city
-    });
+router.post("/profile", function(req, res, next){
+    Profile.create(req.body).then(function(profile){
+        res.send(profile);
+    }).catch(next) ;
+    
 });
 
 // PUT
-router.put("/profile/:id", function(req, res){
+router.put("/profile/:id", function(req, res, next){
     res.send({type: "PUT"});
 });
 
 // DELETE
-router.delete("/profile/:id", function(req, res){
+router.delete("/profile/:id", function(req, res, next){
     res.send({type: "DELETE"});
 });
 
